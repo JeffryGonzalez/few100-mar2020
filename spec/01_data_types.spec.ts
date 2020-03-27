f2c060a697f321752503143174725eba8cdda560
Jeffry Gonzalez <jeff@hypertheory.com>
2020-03-27 09:41:46 -0400

    friday morning1

--- spec/01_data_types.spec.ts
+++ spec/01_data_types.spec.ts
@@ -1,4 +1,18 @@
+import { add, applicationName } from './utils';
 describe('declaring variables in typescript', () => {
+    it('constant might not mean what you think it means', () => {
+        // const means the variable cannot be reassigned to.
+        const PI = 3.1415927;
+        // PI = 3;
+
+        const numbers = [1, 2, 3];
+        // numbers = [9, 2, 3];
+        numbers[0] = 9;
+        const movie = { title: 'Jaws', yearReleased: 1979 };
+        // movie = { title}
+        movie.yearReleased = 1977;
+
+    });
     describe('typing', () => {
         it('implicitly typed variables', () => {
             let name = 'Joe';
@@ -44,3 +58,43 @@ describe('declaring variables in typescript', () => {
 
     });
 });
+
+describe('literals', () => {
+    describe('string literals', () => {
+        it('has them', () => {
+            const message = 'She told me "You look nice today!"';
+            const message2 = 'The Author was Flannery O\'Connor';
+            // tslint:disable-next-line: quotemark
+            const message3 = "The Author was Flannery O'Connor";
+
+            // tslint:disable-next-line: no-console
+            console.log('Is this thing on');
+
+        });
+        it('has template strings', () => {
+            const message = `She told me "You look nice today!"`;
+            const message2 = `The Author was Flannery O'Connor`;
+
+
+            const story = `Life Story
+
+            It was a dark and stormy night. blah blah.
+            The end.
+            `;
+
+            const fragment = `<div>
+            <h1>Hello</h1>
+            </div>`;
+
+            // tslint:disable-next-line: one-variable-per-declaration
+            const name = 'Bob',
+                age = 38;
+
+            const message3 = `The name is ${name} and the age is ${age}`;
+            const message4 = 'The name is ' + name + ' and the age is ' + age;
+            expect(message3).toEqual(message4);
+        });
+    });
+});
+
+
--- spec/utils.ts
+++ spec/utils.ts
@@ -1,1 +1,8 @@
-export const add = (a:number, b:number) => a + b;
+export const add = (a: number, b: number) => a + b;
+
+export const applicationName = 'Super Angular Application';
+
+export class Monkey {
+    public name: string;
+    private age: number;
+}
