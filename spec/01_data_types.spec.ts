656db6e60d7e958568269ea49f0660ce4264a32f
Jeffry Gonzalez <jeff@hypertheory.com>
2020-03-27 11:28:25 -0400

    before lunch

--- spec/01_data_types.spec.ts
+++ spec/01_data_types.spec.ts
@@ -1,4 +1,4 @@
-import { add, applicationName } from './utils';
+import { add, formatName } from './utils';
 describe('declaring variables in typescript', () => {
     it('constant might not mean what you think it means', () => {
         // const means the variable cannot be reassigned to.
@@ -93,6 +93,97 @@ describe('literals', () => {
             const message3 = `The name is ${name} and the age is ${age}`;
             const message4 = 'The name is ' + name + ' and the age is ' + age;
             expect(message3).toEqual(message4);
+        });
+    });
+    it('some various misc. things about literals', () => {
+
+        const n1 = 1;
+        const n2 = 1.3;
+        const n3 = 0xff; // base 16
+        const n4 = 0b01010; // binary
+        const n5 = 0o23; // octal.
+        const big = 1_000_000;
+    });
+});
+
+describe('array literals and tuple types', () => {
+    it('two ways to declare an array', () => {
+
+        const numbers: (number | string)[] = [1, 2, 3, 4];
+        numbers[0] = 'Tacos';
+        // numbers[2] = 'burrito';
+        const val = numbers[2];
+
+        let numbers2: Array<number | string>;
+
+    });
+    it('has array destructuring', () => {
+        //                  0      1       2        3
+        const friends = ['Sean', 'Amy', 'David', 'Sarah'];
+
+        // const f1 = friends[0];
+        // const f2 = friends[1];
+        // const f3 = friends[3];
+        const [f1, f2, , f3] = friends;
+
+        expect(f1).toBe('Sean');
+        expect(f2).toBe('Amy');
+        expect(f3).toBe('Sarah');
+
+        const [first, ...allTheOthers] = friends; // ... "the rest" (rest operator)
+        expect(first).toBe('Sean');
+        expect(allTheOthers).toEqual(['Amy', 'David', 'Sarah']);
+    });
+    it('using immutable techniques to change arrays', () => {
+        const friends = ['Sean', 'Amy', 'David', 'Sarah'];
+        const friends2 = ['Henry', ...friends]; // ... "the spread operator"
+
+        expect(friends2).toEqual(['Henry', 'Sean', 'Amy', 'David', 'Sarah']);
+
+    });
+    describe('a "practical" example of a tuple', () => {
+        it('how non-functional programmers would solve the problem', () => {
+
+            // interface FormatNameResult { formattedName: string; numberOfletters: number; }
+
+
+            // const result = formatName('Han', 'Solo');
+            // OBJECT destructuring
+            const { formattedName, numberOfletters: letters } = formatName('Han', 'Solo');
+
+            expect(formattedName).toBe('Solo, Han');
+            expect(letters).toBe(9);
+
+
+            // const result = formatName('Luke', 'Skywalker');
+
+            // expect(result.formattedName).toBe('Skywalker, Luke');
+
+            const { formattedName: n } = formatName('Lando', 'Calrission');
+            expect(n).toBe('Calrission, Lando');
+        });
+        it('the same thing with tuples', () => {
+
+            function formatName2(first: string, last: string): [string, number] {
+                const name = `${last}, ${first}`;
+                return [name, name.length];
+            }
+
+            const [n, l] = formatName2('Han', 'Solo');
+
+            expect(n).toBe('Solo, Han');
+            expect(l).toBe(9);
+        });
+
+        it('tuple syntax example', () => {
+
+            type Musician = [string, string, number, string];
+
+            const nick: Musician = ['Nick', 'Cave', 63, 'Singer'];
+            const warren: Musician = ['Warren', 'Ellis', 58, 'Violin'];
+
+            expect(warren[2]).toBe(58);
+
         });
     });
 });
--- spec/utils.ts
+++ spec/utils.ts
@@ -1,8 +1,12 @@
 export const add = (a: number, b: number) => a + b;
 
-export const applicationName = 'Super Angular Application';
 
-export class Monkey {
-    public name: string;
-    private age: number;
+
+
+export function formatName(first: string, last: string): { formattedName: string; numberOfletters: number; } {
+    const name = `${last}, ${first}`;
+    return {
+        formattedName: name,
+        numberOfletters: name.length
+    };
 }
